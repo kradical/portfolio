3338529f147f0fe81fdd7b26be360e56a2f58e5c
Konrad Schultz <konrad_here@hotmail.com>
2018-05-31 23:06:16 -0700

    Add css modules.

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -1,4 +1,6 @@
-import * as React from "react";
+import * as React from 'react';
+
+import * as css from './index.css';
 
 interface IHelloProps {
   compiler: string;
@@ -9,9 +11,9 @@ interface IHelloProps {
 export const Hello = (props: IHelloProps) =>
   (
     <React.Fragment>
-      <h1>Hello!</h1>
+      <h1 className={css.foo}>Hello!</h1>
       <p>This site is made with {props.compiler} and {props.framework}.</p>
       <p>Work in progress</p>
-      <small>Brought to you by {props.author}</small>
+      <small className={css.barBaz}>Brought to you by {props.author}</small>
     </React.Fragment>
   );
